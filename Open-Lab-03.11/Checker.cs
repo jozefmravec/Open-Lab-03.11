4d364faf0dc933133b6e81c8c02b39bd6d175653
jozefmravec <jojomravec@gmail.com>
2019-11-25 09:17:16 +0100

    Initial

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            string first = str.Substring(0, str.Length / 2);
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+            return first.Equals(second);
         } 
     }
 }
